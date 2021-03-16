7dbd21bb48e7243371d283638f754fd992c9079b
unknown <andressuper50@gmail.com>
2021-03-16 10:11:08 -0700

    aM2N

---
+++ app/Http/Controllers/NotasController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Notas;
+
+class NotasController extends Controller
+{
+    public function welcome() {
+        return view('welcome');
+    }
+
+    public function index() {
+        $notas = Notas::all(); 
+    
+        return view('notas', ['notas' => $notas]);
+    }
+    
+    public function vistaAgregar() {
+        return view('agregar');
+    }
+
+    public function crear(Request $request){
+        Notas::create([
+            'titulo' => $request -> input('title'),
+            'contenido' => $request -> input ('content'),
+        ]);
+    
+        return redirect('/notas');
+    }
+
+    public function edit($id){
+        $notas = Notas::find($id); //DB::table('notas')->where('id', $id)->first();
+            
+        return view('editar', ['notas' => $notas]);
+            #return 'Aqui se van a editar las notas' .$id;
+    }
+
+    public function update(Notas $notas, Request $request){
+        $notas->update([
+            'titulo' => $request->input('title'),
+            'contenido' => $request->input('content'),
+        ]);
+        return redirect('/notas');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Models\Notas;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*Route::get('notas', function() {
+    $notas = Notas::all(); //DB::table('notas')->get();
+    /*[
+        'primera nota',
+        'segunda nota',
+        'tercera nota',
+        'cuarta nota',
+        'quinta nota',
+    ];
+
+    return view('notas', ['notas' => $notas]);
+})->name('notas.index');*/
+
+Route::get('/', 'App\Http\Controllers\NotasController@welcome')->name('notas.raiz');
+
+Route::get('notas', 'App\Http\Controllers\NotasController@index')->name('notas.index');
+
+Route::get('agregar', 'App\Http\Controllers\NotasController@vistaAgregar')->name('notas.agregar');
+
+Route::post('crear', 'App\Http\Controllers\NotasController@crear')->name('notas.store');
+
+Route::get('notas/{id}/editar', 'App\Http\Controllers\NotasController@edit' )->name('notas.edit');
+
+Route::put('notas/{notas}/editar', 'App\Http\Controllers\NotasController@update' )->name('notas.update');
