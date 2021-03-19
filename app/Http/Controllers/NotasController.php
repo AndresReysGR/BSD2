ea7ecedab34b7cdd91065fc9eb4e8529eda9929e
unknown <andressuper50@gmail.com>
2021-03-19 10:50:07 -0700

    Amen

--- app/Http/Controllers/NotasController.php
+++ app/Http/Controllers/NotasController.php
@@ -44,4 +44,16 @@ class NotasController extends Controller
         ]);
         return redirect('/notas');
     }
+
+    public function destroy($id)
+    {
+        $notas = Notas::find($id);
+
+        $notas->delete();
+
+        return redirect('/notas');
+    }
 }
+
+
+
